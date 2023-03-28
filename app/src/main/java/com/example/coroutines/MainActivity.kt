e3e272c021d4923b0a332202c81c306ee50ac4cc
Francis R Scallatino <tup31915@temple.edu>
2023-03-28 14:57:07 -0400

    pre-activity code example using Thread functionality

--- app/src/main/java/com/example/coroutines/MainActivity.kt
+++ app/src/main/java/com/example/coroutines/MainActivity.kt
@@ -2,10 +2,47 @@ package com.example.coroutines
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.os.Message
+import android.util.Log
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    val timerHandler = Handler(Looper.getMainLooper()){
+
+        timerTextView.text = it.what.toString()
+
+        true
+    }
+
+    val timerTextView: TextView by lazy {
+        findViewById(R.id.timerTextView)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Thread{
+            for(i in 100 downTo 1){
+                Log.d("Countdown", i.toString())
+                Thread.sleep(1000)
+
+                timerHandler.sendEmptyMessage(i)
+
+                /*
+                //this stuff isn't necessary if we're only sending 1 piece of data
+                //in the what variable
+                val msg = Message.obtain()
+
+                msg.what = i
+
+                timerHandler.sendMessage(msg)
+                 */
+            }
+        }.start()
+
     }
 }
